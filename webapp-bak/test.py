88e0cbed51e0a7ff75912b0450f5bd6b48a2c95e
xuequn <505711559@qq.com>
2017-09-12 16:31:56 +0800

    git add

--- webapp-bak/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import os
-from utils import fastwebstatics
-from webapp.models import Charge_info
-
-os.chdir('/usr/local/src/myproject/cdnadmin/webapp/utils')
-print fastwebstatics.get_statics_result()
