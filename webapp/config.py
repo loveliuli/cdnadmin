d9872e31e48d9cf5e1ae5834eb82986dc9d7c36a
loveliuli <qunloveliliu@163.com>
2019-10-22 08:36:25 +0800

    Update config.py
    去掉一些信息

--- webapp/config.py
+++ webapp/config.py
@@ -1,7 +1,7 @@
 class Config(object):
-    SECRET_KEY = '736670cb10a600b695a55839ca3a5aa54a7d7356cdef815d2ad6e19a2031182b'
-    RECAPTCHA_PUBLIC_KEY = "6LdY-RkUAAAAAJmnaYfLWuoWaFe-S97ES28mCHA2"
-    RECAPTCHA_PRIVATE_KEY = '6LdY-RkUAAAAAC_9-pCchurb-GkYcu-4BOC-3K3X'
+    SECRET_KEY = '******'
+    RECAPTCHA_PUBLIC_KEY = '******'
+    RECAPTCHA_PRIVATE_KEY = '******'
 
 
 class ProdConfig(Config):
@@ -11,5 +11,5 @@ class ProdConfig(Config):
 class DevConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:EhqW{OZzS7XX@localhost/cdnadmin?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = '******'
 
